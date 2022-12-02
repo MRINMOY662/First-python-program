566ad9d41ca1e889d6680de62fe48361488fa815
Mrinmoy Deka <103936198+MRINMOY662@users.noreply.github.com>
2022-12-02 20:22:44 +0530

    Add files via upload

---
+++ newfile.py
@@ -0,0 +1,36 @@
+#-----Begining of python------
+
+print('hello world!') #first program of python
+
+name='Juan' '''Changing variable value'''
+name='Nord'
+print('Hello'+ ' '+name)
+
+#------------------------------------
+
+a=3 #getting numerical value
+b=3.3
+d='python'
+e=4.21j
+print(type(a))
+print(type(b))
+print(type(d))
+print(type(e))
+
+#------------------------------------
+
+pi = 3.1415926535897 # this is the pi number
+two_decimals = round(pi, 2)
+three_decimals = round(pi, 3)
+
+print(two_decimals)
+print(three_decimals)
+
+x = pow(4, 2) # same as 4 * 4
+y = pow(4, 3) # same as 4 * 4 * 4
+
+print(x)
+print(y)
+
+print   
+#------------------------------------
